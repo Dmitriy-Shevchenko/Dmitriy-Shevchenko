eb58b8aac9695c26d80be4fa19ab61249425a9d2
Dmity_Shevchenko <shevchenko86@bk.ru>
2020-04-11 14:38:28 +0500

    mod

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+/*
 const gulp = require('gulp');
 const browserSync = require('browser-sync');
 const sass = require('gulp-sass');
@@ -25,3 +26,37 @@ galp.task('watch', function() {
 })
 
 gulp.task('default', galp.parallel('watch','server', 'style')); //Поумолчанию отслеживать и запуска сервер и файлы стилей
+*/
+const gulp        = require('gulp');
+const browserSync = require('browser-sync');
+const sass        = require('gulp-sass');
+const cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
+const rename = require("gulp-rename");
+
+gulp.task('server', function() {
+
+    browserSync({
+        server: {
+            baseDir: "src"
+        }
+    });
+
+    gulp.watch("src/*.html").on('change', browserSync.reload);
+});
+
+gulp.task('styles', function() {
+    return gulp.src("src/sass/**/*.+(scss|sass)")
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename({suffix: '.min', prefix: ''}))
+        .pipe(autoprefixer())
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest("src/css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('watch', function() {
+    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
+})
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
